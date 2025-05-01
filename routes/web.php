2dab85ffef77c317474791b60bd32e213a39d2a2
luis <aramayoluis0@gmail.com>
2025-04-30 22:53:59 -0300

    Version 2.1. Se actualizo la ruta para la api, dejando lista para actualizar mas adelante

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Auth::routes();
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('clientes', ClientController::class)->middleware('auth');
 Route::get('/', [App\Http\Controllers\ClientController::class, 'index'])->middleware('auth');
-Route::get('/clientes/api/auth-user', [ClientApiController::class, 'desktopLogin']);
-Route::get('/clientes/api/update-last-used', [ClientApiController::class, 'updateLastUsed']);
+Route::get('/clientes/api/v1/auth-user', [ClientApiController::class, 'desktopLogin']);
+Route::get('/clientes/api/v1/update-last-used', [ClientApiController::class, 'updateLastUsed']);
